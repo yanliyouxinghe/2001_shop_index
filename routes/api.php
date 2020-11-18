70249e9288004fca280eed60184705a685004dff
wangshuai <1370436059@qq.com>
2020-11-18 09:03:48 +0800

    详情

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::any('/goods/goodsinfo','Api\GoodsController@goods');//详情
+
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//首页
+// 首页
 Route::get('/', function () {
     return view('index.index');
 });
 
+Route::any('/goods/goodsinfo','Index\GoodsController@goodsinfo');//详情
 Route::get('/login','Index\LoginController@login');//登录
 Route::get('/reg','Index\LoginController@reg');//注册
 Route::get('/ser','Index\UserController@ser');//个人中心
