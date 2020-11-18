01c5f29754257fe7ea3ca0aa6ce4c5a659159558
dong <1123370957@qq.com>
2020-11-18 10:15:56 +0800

    解决冲突

--- app/Model/GoodsModel.php
+++ app/Model/GoodsModel.php
@@ -6,24 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class GoodsModel extends Model
 {
-<<<<<<< HEAD
-      protected $table = 'sh_goods';
-=======
+
     protected $table = 'sh_goods';
->>>>>>> a805ec9dd8f56b3f421fa7c5e41cfd7aa0426337
       protected $guarded = [];
       protected $primaryKey = "goods_id";
-
       public $timestamps = false;
-<<<<<<< HEAD
-
       //首页轮播图
+      
       public static function getslicedata(){
             $where['is_show'] = 1;
             return self::select('goods_id','goods_img')->where($where)->take(3)->orderBy('goods_id','desc')->get();
       }
 }
-
-=======
-}
->>>>>>> a805ec9dd8f56b3f421fa7c5e41cfd7aa0426337
