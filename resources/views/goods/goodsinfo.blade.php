23c35b47c7f3a9bee7fa80ae50d2a988c865184c
dong <1123370957@qq.com>
2020-11-25 22:01:30 +0800

    解决冲突

--- resources/views/goods/goodsinfo.blade.php
+++ resources/views/goods/goodsinfo.blade.php
@@ -348,8 +348,6 @@ $(document).ready(function(){
 </html>
 <script src="/static/js/jquery.js"></script>
 <script>
-
-<<<<<<< HEAD
 //个人收藏
 $('.favorIcon').click(function(){
       var goods_id ="{{$goodsinfo[0]['goods_id']}}";
@@ -397,8 +395,6 @@ $('.favorIcon').click(function(){
                 return false;
           }
     }
-=======
->>>>>>> aade1ceb389c3c54cec4cf07de3c8dcaeeb14f40
     $(function(){
   //+号
           $('.plus').click(function(){
