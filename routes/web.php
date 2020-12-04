66d1e3bea9d8e2447ececf76ae0ed45869324fb9
yanliyouxinghe <2483701544@qq.com>
2020-12-04 18:44:40 +0800

    物流

--- routes/web.php
+++ routes/web.php
@@ -99,9 +99,15 @@ Route::post('/orderinfo','Index\OrderController@orderinfo');  //生成订单
 
 Route::get('/userorderlist','Index\UserController@userorderlist');  //生成订单
 Route::post('/changepwd','Index\UserController@changepwd');  //修改密码
-Route::get('/logistics/{id}','Index\LogisticsController@logistics');  //查看物流
+Route::get('/logistics','Index\LogisticsController@logistics');  //查看物流
+// Route::get('/logistics/{id}','Index\LogisticsController@logistics');  //查看物流
+
 
 Route::get('/coupons/{goods_id}','Index\GoodsController@coupons');//优惠券
+
+
+Route::get('/maopao','Index\IndexController@maopao');//优惠券
+
 
 
 });
